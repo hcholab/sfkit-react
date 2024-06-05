872ba2d43fc1906d964b2db92a14e3f6506e2721
Denis Loginov <git@dinvlad.dev>
2024-06-05 15:52:10 -0400

    Use accessToken in useGeneratedAuthHeaders()

--- src/hooks/useGenerateAuthHeaders.tsx
+++ src/hooks/useGenerateAuthHeaders.tsx
@@ -6,7 +6,6 @@ import { useAuth } from "react-oidc-context";
 const useGenerateAuthHeaders = (): Record<string, string> => {
   const { auth_key } = useParams();
   const auth = useAuth();
-  const idToken = auth.user?.id_token || "";
   const accessToken = auth.user?.access_token || "";
   const { apiBaseUrl } = useContext(AppContext);
 
@@ -16,16 +15,14 @@ const useGenerateAuthHeaders = (): Record<string, string> => {
     const headers: Record<string, string> = {
       "Content-Type": "application/json",
     };
-    console.log('access_token', accessToken);
-    console.log('idToken', idToken);
-    if (idToken) {
-      headers.Authorization = `Bearer ${idToken}`;
+    if (accessToken) {
+      headers.Authorization = `Bearer ${accessToken}`;
     } else if (auth_key && !onTerra) {
       headers.Authorization = auth_key;
     }
 
     return headers;
-  }, [auth_key, idToken, accessToken, apiBaseUrl]);
+  }, [auth_key, accessToken, apiBaseUrl]);
 };
 
 export default useGenerateAuthHeaders;
